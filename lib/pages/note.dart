d424c37fdd59993fc30768492744a4ec1b10dadd
Ashit Adhikari <as.adhikari@outlook.com>
2018-05-10 15:36:55 -0500

    Note Database Provider implemented in note page.

--- lib/pages/note.dart
+++ lib/pages/note.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import '../database/database.dart';
+import 'package:path/path.dart';
+import '../database/note_database_provider.dart';
 
 class NotePage extends StatelessWidget
 {
   final String pageText = "NotePage";
-  final AppDatabase appDatabase;
+  final NoteDatabaseProvider noteDatabaseProvider;
 
   NotePage(String _databaseDirectory, String _databaseName)
-      : this.appDatabase = new AppDatabase(_databaseDirectory, _databaseName);
+      : this.noteDatabaseProvider = new NoteDatabaseProvider(join(_databaseDirectory, _databaseName))
+      {
+        this.noteDatabaseProvider.setUpDatabase();
+      }
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return new Scaffold(
-        body: new Center(
-          child: new Text(pageText + " " + this.appDatabase.getDatabaseFullPath())
-        ),
+      body: new Center(
+          child: new Text(pageText)
+      ),
     );
   }
 }
