c7c9752156df5dd87266c3233569715946e1f56f
Ashit Adhikari <as.adhikari@outlook.com>
2018-05-11 11:19:41 -0500

    flutter format lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import './pages/home.dart';
 
-void main() => runApp(
-  new MaterialApp(
-    home: new SelfNote()
-  )
-);
+void main() => runApp(new MaterialApp(home: new SelfNote()));
 
 class SelfNote extends StatefulWidget {
   @override
   HomePage createState() => new HomePage();
 }
-
--- lib/pages/category.dart
+++ lib/pages/category.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import '../database/database.dart';
 
-class CategoryPage extends StatelessWidget
-{
+class CategoryPage extends StatelessWidget {
   final String pageText = "CategoriesPage";
   final AppDatabase appDatabase;
 
   CategoryPage(String _databaseDirectory, String _databaseName)
-    : appDatabase = new AppDatabase(_databaseDirectory, _databaseName);
+      : appDatabase = new AppDatabase(_databaseDirectory, _databaseName);
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return new Scaffold(
       body: new Center(
-          child: new Text(pageText + " " + this.appDatabase.getDatabaseFullPath())
-      ),
+          child: new Text(
+              pageText + " " + this.appDatabase.getDatabaseFullPath())),
     );
   }
 }
