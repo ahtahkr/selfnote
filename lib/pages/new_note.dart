0794131fe6337921b04e26f3a29123137ad1cc1d
Ashit Adhikari <as.adhikari@outlook.com>
2018-05-14 11:35:08 -0500

    Close the New Note page by clicking buttons.

--- lib/pages/new_note.dart
+++ lib/pages/new_note.dart
@@ -9,12 +9,32 @@ class NewNoteWidget extends StatefulWidget {
 
 class NewNoteWidgetState extends State<NewNoteWidget> {
 
+  void _close()
+  {
+      Navigator.pop(context);
+  }
+
+  void _save()
+  {
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("This is the new note widget."),
       ),
+      body: new Center(
+      child: Column(
+    children: <Widget>[
+      new RaisedButton(onPressed: _save,
+        child: const Text("Save"),
+        color: Colors.green,),
+        new RaisedButton(onPressed: _close,
+          child: const Text("Cancel"),
+          color: Colors.red,)
+      ]))
     );
   }
 }
