2837b4d0fe0e7d004ff882f22b2a7133f2f30008
Ashit Adhikari <as.adhikari@outlook.com>
2018-05-23 13:18:45 -0500

    Categories dropdown added to the new note page.

--- lib/pages/new_note.dart
+++ lib/pages/new_note.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import '../database/note_database_provider.dart';
+import '../database/category_database_provider.dart';
+import '../database/modal/category.dart';
 import 'dart:math' as math;
 
 class NewNoteWidget extends StatefulWidget {
   final String _databaseFullPath;
-
   NewNoteWidget(String databaseFullPath)
       : this._databaseFullPath = databaseFullPath;
-
   @override
   createState() => new NewNoteWidgetState(this._databaseFullPath);
 }
@@ -16,20 +16,23 @@ class NewNoteWidgetState extends State<NewNoteWidget>
     with TickerProviderStateMixin {
   TextEditingController _textEditController;
   NoteDatabaseProvider _noteDatabaseProvider;
+  CategoryDatabaseProvider _categoryDatabaseProvider;
+  List<Category> categories;
+  Category selectedCategory;
   AnimationController _controller;
   Note note;
-
   static const List<IconData> icons = const [Icons.save, Icons.cancel];
   static const List<Color> _backgroundColor = const [
     Colors.green,
     Colors.orange
   ];
-
   NewNoteWidgetState(String databaseFullPath) {
     this._noteDatabaseProvider = new NoteDatabaseProvider(databaseFullPath);
+    this._categoryDatabaseProvider =
+        new CategoryDatabaseProvider(databaseFullPath);
     this.note = new Note();
+    this.categories = new List();
   }
-
   void _cancel() {
     Navigator.pop(context, false);
   }
@@ -37,7 +40,7 @@ class NewNoteWidgetState extends State<NewNoteWidget>
   void _save() {
     print(_textEditController.value.text.toString());
     this.note.message = _textEditController.value.text.toString();
-
+    this.note.categoryId = this.selectedCategory.id;
     this._noteDatabaseProvider.open().then((_bool) {
       if (_bool) {
         this._noteDatabaseProvider.insertUpdate(this.note).then((new_note) {
@@ -62,12 +65,26 @@ class NewNoteWidgetState extends State<NewNoteWidget>
 
   @override
   void initState() {
+    print('NewNoteWidgetState initState start.');
     super.initState();
     _textEditController = new TextEditingController();
     _controller = new AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 500),
     );
+    this._categoryDatabaseProvider.initialSetUp().then((res) {
+      print('initial setup complete.');
+      this._categoryDatabaseProvider.get().then((res_1) {
+        print('get complete. ' + res_1.toString());
+        setState(() {
+          this.categories = res_1;
+          this.selectedCategory = this.categories[0];
+          print('NewNoteWidgetState initState assigning selectedCategory. ' +
+              this.selectedCategory.toString());
+        });
+      });
+    });
+    print('NewNoteWidgetState initState end.');
   }
 
   _dismissKeyboard() {
@@ -89,31 +106,49 @@ class NewNoteWidgetState extends State<NewNoteWidget>
     print(index);
   }
 
+  _categorySelected(Category category) {
+    setState(() {
+      this.selectedCategory = category;
+      print('categorySelected a: ' + category.toString());
+      print('categorySelected b: ' + this.selectedCategory.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('NewNoteWidgetState build start.');
     Color foregroundColor = Colors.white;
-
     InputDecoration inputDecoration;
     if (note.message != null && note.message.length > 0) {
       this._textEditController.text = note.message.toString();
     } else {
       inputDecoration = new InputDecoration(hintText: 'Enter Description');
     }
-
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("This is the new note widget."),
           automaticallyImplyLeading: false,
         ),
-        body: new SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: new TextField(
-            decoration: inputDecoration,
-            maxLines: null,
-            autofocus: true,
-            controller: _textEditController,
-          ),
-        ),
+        body: new Column(children: <Widget>[
+          new DropdownButton(
+              isDense: true,
+              value: this.selectedCategory,
+              items: this.categories.map((Category category) {
+                return new DropdownMenuItem(
+                    value: category, child: new Text(category.title));
+              }).toList(),
+              onChanged: (Category category) {
+                _categorySelected(category);
+              }),
+          new SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: new TextField(
+                decoration: inputDecoration,
+                maxLines: null,
+                autofocus: true,
+                controller: _textEditController,
+              ))
+        ]),
         floatingActionButton: new Column(
             mainAxisSize: MainAxisSize.min,
             children: new List.generate(icons.length, (int index) {
